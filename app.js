49efc5df2a07ea87594b4205ec351024bd2a9eac
MattettaM <Mattias@10.132.155.195>
2016-02-09 12:33:15 +0100

    Attempt at connection establishment.

--- app.js
+++ app.js
@@ -28,3 +28,17 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 	console.log("server starting on " + appEnv.url);
 });
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host	: 'us-cdbr-iron-east-03.cleardb.net',
+	user	: 'beffb49c35eb8a',
+	password: '00145932'
+});
+
+connection.connect(function(err){
+	if (err){
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
