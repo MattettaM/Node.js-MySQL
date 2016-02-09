7e55774a21b2fd206fa866fd403e78b589e3b1cb
MattettaM <Mattias@10.132.155.195>
2016-02-09 12:14:57 +0100

    Re-Do, scripts already present

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+	console.log("server starting on " + appEnv.url);
 });
 
